c3f30726eb41382984a979ac6c68ac14d07426e2
Anessa Petteruti <anessa_petteruti@brown.edu>
2023-06-25 15:11:39 -0400

    Initial Commit

---
+++ Vinci/VinciApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinciApp.swift
+//  Vinci
+//
+//  Created by Anessa Petteruti on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct VinciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
