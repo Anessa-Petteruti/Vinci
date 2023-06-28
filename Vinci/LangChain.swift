28861a98877ed8fd0600793bb953e4e3c905bd8e
BenBachmann <ben_bachmann@brown.edu>
2023-06-28 19:30:40 +0100

    added langchain file

---
+++ Vinci/LangChain.swift
@@ -0,0 +1,8 @@
+
+
+let query = "please point out the water bottle"
+
+let agent = initialize_agent(llm: llm, tools: [TextResponseTool(), BoundingBoxTool()])
+let answer = await agent.run(args: query)
+print(answer) // should draw a bounding box around the water bottles AND say something like "sure, I've here are all the boxes in the frame"
+
