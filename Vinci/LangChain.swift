3b02cc273d7d0a73e4c57bdf4c0de8d135150949
BenBachmann <ben_bachmann@brown.edu>
2023-06-28 19:45:10 +0100

    added langchain file

--- Vinci/LangChain.swift
+++ Vinci/LangChain.swift
@@ -1,10 +1,31 @@
 
-
+import Foundation
 // need to define TextResponseTool() and BoundingBoxTool()
+public struct BoundingBoxTool: BaseTool {
+    public init() {}
+    public func name() -> String {
+        "Bounding box"
+    }
+    
+    public func description() -> String {
+        "useful for drawing attention to an object by drawing a bounding box around it"
+    }
+    
+    public func _run(args: String) throws -> nil {
+        DispatchQueue.main.async {
+        self.setupDetector()
+        self.setupLayers()
+    }
+    // Return a success message or any relevant result
+    return "Bounding box detection started."
+    }
+    
+    
+}
 
 let query = "please point out the water bottle"
 
 let agent = initialize_agent(llm: llm, tools: [TextResponseTool(), BoundingBoxTool()])
 let answer = await agent.run(args: query)
-print(answer) // should draw a bounding box around the water bottles AND say something like "sure, I've here are all the boxes in the frame"
+print(answer) // should draw a bounding box around the water bottles AND print something like "sure, I've here are all the boxes in the frame"
 
